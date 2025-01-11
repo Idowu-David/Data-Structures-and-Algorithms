10a2370df3d3adfc814624364fc1999e39678a30
Idowu-David <Idowu-David@github.com>
2025-01-11 14:17:55 +0100

    Fix: insert at position

--- Doubly Linked LIst/dll.py
+++ Doubly Linked LIst/dll.py
@@ -37,8 +37,38 @@ class DoublyLinkedList:
         
     def insert_at_position(self, value, position):
         """Insert a node at a specified position"""
-        pass
-    
+        if position > self.size:
+            raise ValueError("Position greater than size of the list")
+        if position < 0:
+            raise ValueError("Position cannot be negative")
+            
+        new = Node(value)
+        if position == 0:   # Add node at the beginning
+            if self.head == None:   # List is empty
+                self.head = new
+            else:
+                new.next = self.head
+                self.head.prev = new
+                self.head = new
+        else:
+            current = self.head
+            for _ in range(position):
+                prev = current
+                current = current.next
+            if not current:
+                prev.next = new
+                new.prev = prev
+                self.tail = new
+            else:
+                prev = current.prev
+                new.next = current
+                current.prev = new
+                new.prev = prev
+                prev.next = new
+        self.size += 1
+            
+                
+                    
     def __str__(self):
         """Prints the singly linked list"""
         current = self.head
@@ -50,19 +80,16 @@ class DoublyLinkedList:
         st += "None"
         return st
     
+
+ # Initialize the list
 node = DoublyLinkedList()
-node.insert_at_beginning(9)
-node.insert_at_end(8)
-node.insert_at_end(7)
-node.insert_at_end(4)
+node.insert_at_position(3, 0)
+print(node)
+node.insert_at_position(2, 1)
+print(node)
 node.insert_at_beginning(8)
 print(node)
-print("Size: ", node.size)
-    
-    
-
-    
-
-
-    
-        
+node.insert_at_end(4)
+print(node)
+node.insert_at_position(1, 3)
+print(node)
